ce0060ac3d8c844a4db3f8f193eed0312428fe83
wequbles <120826725+wequbles@users.noreply.github.com>
2023-01-02 15:15:16 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(".owl-carousel").owlCarousel({
+	autoWidth: true,
+	loop: true,
+	mouseDrag: true,
+	touchDrag: true,
+	smartSpeed: 2500000,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	autoplayTimeout: 1500
+});
